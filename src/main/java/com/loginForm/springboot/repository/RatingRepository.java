5aa1ea8c8f240f6be5c16da0beada1a02e8d0233
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-09-19 12:38:50 +0530

    Delete src/main/java/com/loginForm/springboot/repository/RatingRepository.java

--- src/main/java/com/loginForm/springboot/repository/RatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.loginForm.springboot.repository;
-
-import com.loginForm.springboot.model.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-}
