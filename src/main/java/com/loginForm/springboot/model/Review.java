d1143fc276b3e4b0d38f62039896e52ddb350189
lisyjayakumar <122440078+lisyjayakumar@users.noreply.github.com>
2024-09-19 12:35:39 +0530

    Add files via upload

---
+++ src/main/java/com/loginForm/springboot/model/Review.java
@@ -0,0 +1,61 @@
+package com.loginForm.springboot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String review;
+    private int rating;
+
+    // Constructors
+    public Review() {}
+
+    public Review(String name, String review, int rating) {
+        this.name = name;
+        this.review = review;
+        this.rating = rating;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
+	
